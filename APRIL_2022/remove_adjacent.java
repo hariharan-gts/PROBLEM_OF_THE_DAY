4f1d803a7d57d3b0be3cfa1b7ae9770f80a8ac01
hariharan-gts <harihh03@gmail.com>
2022-04-06 18:16:58 +0530

    potd2

---
+++ APRIL_2022/remove_adjacent.java
@@ -0,0 +1,38 @@
+package APRIL_2022;
+//05-04-2022
+import java.util.*;
+
+
+public class remove_adjacent {
+    public static  String remove(String s) {
+    	HashMap<Character,Integer>hs=new HashMap<>();
+    	String ans="";
+    	for(int i=0;i<s.length();i++) {
+    		if(!hs.containsKey(s.charAt(i))) {
+    		hs.put(s.charAt(i), 1);	
+    		}else {
+    			hs.put(s.charAt(i), hs.get(s.charAt(i))+1);
+    		}
+    	}
+    	for(Map.Entry<Character,Integer>e:hs.entrySet()) {
+    		char k=e.getKey();
+    		int v=e.getValue();
+    		hs.put(k, v%2);
+    	}
+    	for(int i=0;i<s.length();i++) {
+    		if(hs.get(s.charAt(i))==1) {
+    			ans+=s.charAt(i);
+    			hs.put(s.charAt(i), 0);
+    		}
+    	}
+    		
+    	return ans.length()==0?"Empty":ans;
+    }
+  
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		System.out.println(remove(s));
+	}
+
+}
