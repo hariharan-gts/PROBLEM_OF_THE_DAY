344df4dc25f724aa169b74c7832ae3457198a925
hariharan-gts <harihh03@gmail.com>
2022-04-07 21:59:51 +0530

    potd2

--- APRIL_2022/add_two_array.java
+++ APRIL_2022/add_two_array.java
@@ -11,7 +11,7 @@ public class add_two_array {
     	if(n1==n2)
     	 n3=n1+1;
     	else
-    	  n3=Math.max(n2, n1);
+    	  n3=Math.max(n2, n1)+1;
     	int ans[]=new int[n3];
     	int c=0;
     	int s=0;
@@ -24,21 +24,26 @@ public class add_two_array {
             i--;
             j--;
         }
-        if(c!=0) {
-    		ans[k]=c;
-    	}
+        
     	while(i>=0) {
-    		ans[k--]+=a[i--];
+    		s=a[i--]+c;
+    		ans[k--]=s%10;
+    		c=s/10;
     	}
     	while(j>=0) {
-    		ans[k--]+=b[j--];
+    		s=b[j--]+c;
+    		ans[k--]=s%10;
+    		c=s/10;
+    	}
+    	if(c!=0) {
+    		ans[k]=c;
     	}
     	
     	return Arrays.toString(ans);
     }
 	public static void main(String[] args) {
-		int a[]= {1,2};
-		int b[]= {9,9};
+		int a[]= {9,9,9};
+		int b[]= {1};
 		System.out.println(add(a,b));
 	}
 
